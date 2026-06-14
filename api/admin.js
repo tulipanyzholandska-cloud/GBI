da601336154b4dc6c85d3659d00a8400c5791ba4
tulipanyzholandska-cloud <tulipanyzholandska@gmail.com>
2026-06-14 11:14:08 +0200

    admin: add env var diagnostics for 500 debug

--- api/admin.js
+++ api/admin.js
@@ -13,6 +13,9 @@ export default async function handler(req, res) {
   }
 
   try {
+    if (!process.env.SUPABASE_URL) throw new Error('SUPABASE_URL not set');
+    if (!process.env.SUPABASE_ANON_KEY) throw new Error('SUPABASE_ANON_KEY not set');
+
     const supabase = createClient(
       process.env.SUPABASE_URL,
       process.env.SUPABASE_ANON_KEY
